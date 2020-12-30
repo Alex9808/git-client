d25e44e99c8647c9b797c5f48c701eaf18f5d934
Alex9808 <alex_az9808@hotmail.com>
2020-12-30 17:31:18 -0600

    Connect CommitView to Redux State

--- src/views/CommitView/CommitView.jsx
+++ src/views/CommitView/CommitView.jsx
@@ -1,15 +1,25 @@
 import React, {Component} from "react";
 import PropTypes from 'prop-types';
-import {withStyles, Typography} from "@material-ui/core";
+import {withStyles, Typography, Paper} from "@material-ui/core";
+import {bindActionCreators} from "redux";
+import {fetchCommit} from "../../actions";
+import {connect} from "react-redux";
 
 const styles = theme => ({});
 
 class CommitView extends Component {
     render() {
-        const {classes} = this.props;
+        const {classes, author, commitSha, date, message, tree} = this.props;
         return (
             <>
-                <Typography>Commit info</Typography>
+                <Helmet>
+                    <title>{message}</title>
+                </Helmet>
+                <Paper style={{padding: 24}}>
+                    <Typography variant={"subtitle1"}>Realizada
+                        por: <b>{author.name || ''} ({author.email || ''})</b> el día {new Date(date).toLocaleString()}
+                    </Typography>
+                </Paper>
             </>
         )
     }
@@ -17,6 +27,23 @@ class CommitView extends Component {
 
 CommitView.propTypes = {
     classes: PropTypes.object,
+    commitSha: PropTypes.string,
+    message: PropTypes.string,
+    date: PropTypes.string,
+    author: PropTypes.object,
+    tree: PropTypes.array,
+    fetchCommit: PropTypes.func,
 };
 
-export default withStyles(styles)(CommitView);
+const mapStateToProps = state => ({
+    commitSha: state.commits.commit.commitSha,
+    message: state.commits.commit.message,
+    date: state.commits.commit.date,
+    author: state.commits.commit.author,
+    tree: state.commits.commit.tree,
+
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators({fetchCommit}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(CommitView));
